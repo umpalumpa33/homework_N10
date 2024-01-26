73ca3d11771f8ec51fd952558564402e3fa64396
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2024-01-26 17:37:19 +0400

    daiwera?

---
+++ script.js
@@ -0,0 +1,40 @@
+// 1.
+// function TransformIntoIfElse(browser) {
+//     if (browser == 'Edge'){
+//         alert("You've got the Edge!")
+//     } else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera') {
+//         alert('Okay we support these browsers too')
+//     } else {
+//         alert('We hope that this page looks ok')
+//     }
+// };
+
+
+// 2.
+// num1 = parseInt(prompt("შეიყვანე პირველი რიცხვი: "))
+// num2 = parseInt(prompt("შეიყვანე მეორე რიცხვი: "))
+// operatori = prompt("შეიყვანე ოპერატორი: ")
+// function Calculator(num1, num2, operatori){
+//     if (operatori == '*'){
+//         alert(`ამ ორი რიცხვის ნამრავლია: ${num1 * num2}`)
+//     } else if(operatori == '+'){
+//         alert(`ამ ორი რიცხვის ჯამია: ${num1 + num2}`)
+//     } else if(operatori == '-'){
+//         alert(`ამ ორი რიცხვის სხვაობაა: ${num1 - num2}`)
+//     } else if(operatori == '/'){
+//         alert(`ამ ორი რიცხვის შეფარდებაა: ${num1 / num2}`)
+//     } 
+// }
+// Calculator(num1, num2, operatori)
+
+// 3.
+// ricxvi = prompt("შეიყვანე რიცხვი: ")
+// function IsNumberOrNot(i){
+//     if(isNaN(i)){
+//         alert('ეს არ არის რიცხვი!!!')
+//     }
+// }
+// IsNumberOrNot(ricxvi)
+
+// 4.
+ricxvi = prompt("შეიყვანე რიცხვი: ")
